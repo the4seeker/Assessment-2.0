a9210df3e11ecfc1951fb9e94d95977f4fec5918
Takudzwa <tmunyoro@hotmail.co.uk>
2024-09-15 11:47:41 +0100

    Initial Commit

---
+++ Assessment 2.0/Assessment_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Assessment_2_0App.swift
+//  Assessment 2.0
+//
+//  Created by Takudzwa Munyoro on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Assessment_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assessment 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assessment 2.0
+//
+//  Created by Takudzwa Munyoro on 15/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
